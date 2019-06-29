f40e3a480fb3d5697d58dfc022f3ea3f79ceef15
Tom McKeesick <tmck01@gmail.com>
2019-06-29 15:13:05 +1000

    Add dir structure and trie module

--- punsy/punsy.py
+++ punsy/punsy.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
-import random
 import sys
-from nltk.corpus.reader import cmudict
+from structs.trie import Trie
 
 def parse_cmu(istream):
     for i, line in enumerate(istream):
         try:
-            yield [i, *line.decode('ISO-8859-1').strip().split('  ')]
+            yield [i, *line.decode('ISO-8859-1').strip().split('|')]
         except UnicodeDecodeError as e:
             print(line)
             raise e
 
 def run(ifpath):
+    t = Trie()
     with open(ifpath, 'rb') as istream:
         for i, word, phonemes in parse_cmu(istream):
-            print(i, word, phonemes)
+            print(i, word, phonemes.split(' '))
+            t.insert(''.join(phonemes.split(' ')), word)
+    return t
+
 
 if __name__ == '__main__':
     run(sys.argv[1])
