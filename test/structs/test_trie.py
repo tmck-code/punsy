360e3a408dc7935fbba6e9f9e1d617dda0946518
Tom McKeesick <tmck01@gmail.com>
2019-07-15 01:23:06 +1000

    Made some unit tests to make sure that trie works

---
+++ test/structs/test_trie.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+from punsy.structs.trie import Trie
+
+class TestTrie(unittest.TestCase):
+
+    def setUp(self):
+        self.words = {
+            'NONE': ['N', 'AH1', 'N'],
+            'ONCE': ['W', 'AH1', 'N', 'S'],
+            'NAAN': ['N', 'AH1', 'N']
+        }
+        self.trie = Trie()
+        for word, pronunciation in self.words.items():
+            self.trie.insert(pronunciation, word)
+
+    def testInsert(self):
+        self.assertEqual({'NONE', 'NAAN'}, self.trie['N', 'AH1', 'N'].data)
+        self.assertEqual({'ONCE'}, self.trie['W', 'AH1', 'N', 'S'].data)
+
+    def testGetItem(self):
+        trie = Trie()
+        trie.insert('A', 123)
+        self.assertEqual(trie, trie['A'])
+
