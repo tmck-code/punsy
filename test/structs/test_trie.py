61ec3b82d1583cb787278359e34218ade4627c75
Tom McKeesick <tmck01@gmail.com>
2019-07-15 09:02:29 +1000

    Start draft unittests for suffix trie

--- test/structs/test_trie.py
+++ test/structs/test_trie.py
@@ -11,13 +11,36 @@ class TestTrie(unittest.TestCase):
     def setUp(self):
         self.words = {
             'NONE': ['N', 'AH1', 'N'],
-            'ONCE': ['W', 'AH1', 'N', 'S'],
             'NAAN': ['N', 'AH1', 'N'],
-            'PUN': ['P', 'AH1', 'N'],
+            'ONCE': ['W', 'AH1', 'N', 'S'],
+            'PUN':  ['P', 'AH1', 'N'],
         }
         self.trie = Trie()
         for word, pronunciation in self.words.items():
             self.trie.insert(pronunciation, word)
+
+    def test_insert_suffixes(self):
+        words = [
+            'cat',
+            'bat',
+            'catinthehat',
+            'rat',
+        ]
+        struct = Trie(key_reversed=True)
+        for word in words:
+            struct.insert(word)
+
+
+        result = struct['at']
+        self.assertEqual(set('a'), result.value)
+        self.assertEqual(['c', 'b', 'h', 'r'], list(result.children.keys()))
+        # cat, bat & rat are all 'final'
+        self.assertTrue(result.children['c'].final)
+        self.assertTrue(result.children['b'].final)
+        self.assertTrue(result.children['r'].final)
+        # h should contain 'catinthehat'
+        # self.assertFalse(result.children['h'].final)
+        # print(result.children['h']['e']['h'])
 
     def testInsert(self):
         self.assertEqual({'NONE', 'NAAN'}, self.trie['N', 'AH1', 'N'].data)
@@ -27,6 +50,6 @@ class TestTrie(unittest.TestCase):
     def testGetItem(self):
         trie = Trie()
         trie.insert('A', 123)
-        print(trie['A'])
-        self.assertEqual(123, trie['A'].value)
+        self.assertEqual({123}, trie['A'].data)
+        self.assertTrue('A' in trie['A'].value)
 
