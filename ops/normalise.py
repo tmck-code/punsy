0ab551b1142484e6f25e5567222c84c1959ca375
Tom McKeesick <tmck01@gmail.com>
2019-08-09 00:39:42 +1000

    Improve normalising file entries and user messages

--- ops/normalise.py
+++ ops/normalise.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, re
 
 def parse_cmu(istream):
     for i, line in enumerate(istream):
         try:
-            raw = line.decode('ISO-8859-1')
-            if should_skip(raw):
-                continue
-            yield [i, *raw.strip().split('  ')]
+            entry = line.decode('ISO-8859-1').strip().split(' ' * 2)
+            if not should_skip(entry):
+                yield entry
         except UnicodeDecodeError as e:
             print(line)
             raise e
 
-def should_skip(raw):
-    if raw.startswith(';;') or '(1)' in raw:
-        return True
-    return False
+def should_skip(entry):
+    skip = False
+    if entry[0].startswith(';;') or re.match(r'(\d)', entry[0]):
+        print(f'Skipping non-word or multiple definition: {entry}')
+        skip = True
+    if not entry[0][0].isalpha():
+        print(f'Skipping probable non-word: {entry}')
+        skip = True
+    elif len(entry) != 2:
+        print(f'Skipping empty entry: {entry}')
+        skip = True
+    return skip
 
-def run(ifpath):
-    with open(ifpath, 'rb') as istream, open(ifpath + '.utf8', 'w') as ostream:
-        for i, word, phonemes in parse_cmu(istream):
-            print(i, word, phonemes.split(' '))
+def run(ifpath, ofpath):
+    with open(ifpath, 'rb') as istream, open(ofpath, 'w') as ostream:
+        for word, phonemes in parse_cmu(istream):
             ostream.write('|'.join([word, phonemes]) + '\n')
 
 if __name__ == '__main__':
-    run(sys.argv[1])
+    run(sys.argv[1], sys.argv[2])
